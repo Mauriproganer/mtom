b63fea3f48e6b21c94ff8d6632848009dcbb61b6
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-25 16:04:14 +0200

    Refactor Firebase config and initialization
    Updated Firebase configuration to use placeholder values and modified initialization for server-side rendering compatibility.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
-  authDomain: "m-to-m-bc172.firebaseapp.com",
-  projectId: "m-to-m-bc172",
-  storageBucket: "m-to-m-bc172.firebasestorage.app",
-  messagingSenderId: "787761267311",
-  appId: "1:787761267311:web:38b8512c36b14a3557039a",
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID",
 };
 
-// INIT
-const app = initializeApp(firebaseConfig);
+// 🔥 evitar error en Vercel (SSR)
+let app: any;
 
-// EXPORT DB
-export const db = getFirestore(app);
+if (typeof window !== "undefined") {
+  app = initializeApp(firebaseConfig);
+}
+
+export const db = typeof window !== "undefined" ? getFirestore(app) : null;
