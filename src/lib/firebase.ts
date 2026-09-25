bf27716372d7f82b683448930214709aee850511
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-25 15:47:31 +0200

    Update Firebase configuration and fix export statement

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
   authDomain: "m-to-m-bc172.firebaseapp.com",
   projectId: "m-to-m-bc172",
-  storageBucket: "m-to-m-bc172.firebasestorage.app",
+  storageBucket: "m-to-m-bc172.appspot.com",
   messagingSenderId: "787761267311",
   appId: "1:787761267311:web:38b8512c36b14a3557039a"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
