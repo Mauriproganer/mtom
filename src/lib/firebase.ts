8e0685198f5c2a7e415ffa74dc961f97deb037f9
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-25 16:01:42 +0200

    Update Firebase storage bucket URL

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
   authDomain: "m-to-m-bc172.firebaseapp.com",
   projectId: "m-to-m-bc172",
-  storageBucket: "m-to-m-bc172.appspot.com",
+  storageBucket: "m-to-m-bc172.firebasestorage.app",
   messagingSenderId: "787761267311",
-  appId: "1:787761267311:web:38b8512c36b14a3557039a"
+  appId: "1:787761267311:web:38b8512c36b14a3557039a",
 };
 
+// INIT
 const app = initializeApp(firebaseConfig);
+
+// EXPORT DB
 export const db = getFirestore(app);
