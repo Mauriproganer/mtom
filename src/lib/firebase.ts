5be68a9da54c4e862882b90f6a934c0253642ea4
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-26 01:12:45 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
+  authDomain: "m-to-m-bc172.firebaseapp.com",
+  projectId: "m-to-m-bc172",
+  storageBucket: "m-to-m-bc172.appspot.com",
+  messagingSenderId: "787761267311",
+  appId: "1:787761267311:web:38b8512c36b14a3557039a"
 };
 
 const app = initializeApp(firebaseConfig);
