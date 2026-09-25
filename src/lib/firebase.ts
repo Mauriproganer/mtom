9e5b1193808396ec9aeb0311d20809a75859aaf3
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-25 15:19:45 +0200

    Update Firebase API key in firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
+  apiKey: "TU_API_KEY",
   authDomain: "m-to-m-bc172.firebaseapp.com",
   projectId: "m-to-m-bc172",
   storageBucket: "m-to-m-bc172.firebasestorage.app",
   messagingSenderId: "787761267311",
-  appId: "1:787761267311:web:38b8512c36b14a3557039a",
-  measurementId: "G-ZEP3FM90S9"
+  appId: "1:787761267311:web:38b8512c36b14a3557039a"
 };
 
 const app = initializeApp(firebaseConfig);
