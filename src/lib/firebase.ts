d9880ca0564694873ea311cd681dbbb2ca42fee8
pepitopro <145064743+Mauriproganer@users.noreply.github.com>
2026-09-26 15:05:01 +0200

    Refactor Firebase initialization and config
    Updated firebase configuration and modified app initialization logic to check for window context.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1UXQ__7lBW_FiDIbLjlsz8lWJWwyr85Q",
+  apiKey: "AIzaSyC1UXQ__7lBW_FiDlBljlsz8lWJwyvr85Q",
   authDomain: "m-to-m-bc172.firebaseapp.com",
   projectId: "m-to-m-bc172",
   storageBucket: "m-to-m-bc172.appspot.com",
   messagingSenderId: "787761267311",
-  appId: "1:787761267311:web:38b8512c36b14a3557039a"
+  appId: "1:787761267311:web:38b8512c36b14a3557039a",
 };
 
-const app = initializeApp(firebaseConfig);
+let app: any;
 
-export const db = getFirestore(app);
+if (typeof window !== "undefined") {
+  app = initializeApp(firebaseConfig);
+}
+
+export const db =
+  typeof window !== "undefined" ? getFirestore(app) : null;
